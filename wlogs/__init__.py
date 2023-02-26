0e3df68b1048af6a606e0e26edf2ba25c29be253
Matt Matt <matthieu.chambery@gmail.com>
2023-02-26 15:03:14 +0100

    guild id declare in wlogs init file

--- wlogs/__init__.py
+++ wlogs/__init__.py
@@ -1,2 +1,6 @@
 from .api import GraphQLClient, graphQL_client_from_json
 from .file_manager import WowPaths, save_guild_reports_to_json
+
+
+# TODO : create conf file with guild information
+GUILD_ID = 505778
