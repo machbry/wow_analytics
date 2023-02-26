eb21f33a2e6cd340acc3b024a3dc65b4d39f415f
Matt Matt <matthieu.chambery@gmail.com>
2023-02-26 14:19:59 +0100

    when saving files, create missing directories and save a copy in latest

--- wlogs/file_manager.py
+++ wlogs/file_manager.py
@@ -1,14 +1,15 @@
 from enum import Enum
 import os
+from typing import List
 from pathlib import Path
 import json
 from datetime import datetime
 
 
 DATE_FORMAT = "%d-%m-%Y"
+LATEST_DIR_NAME = "latest"
 
 
-# TODO : create these directories if they don't exist
 class WowPaths(Enum):
     BASE_PATH = Path(__file__).parent.parent.resolve()
     DATA = os.path.join(BASE_PATH, "data")
@@ -19,7 +20,23 @@ def now_to_string() -> str:
     return datetime.now().strftime(DATE_FORMAT)
 
 
+def check_and_create_dirs(directories: List[Path]):
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+
 def save_guild_reports_to_json(content, guild_id: int):
-    file_path = os.path.join(WowPaths.REPORTS.value, f"{str(guild_id)}_reports_{now_to_string()}.json")
+    reports_dir = WowPaths.REPORTS.value
+    reports_latest_dir = os.path.join(reports_dir, LATEST_DIR_NAME)
+
+    file_path = os.path.join(reports_dir, f"{str(guild_id)}_reports_{now_to_string()}.json")
+    latest_file_path = os.path.join(reports_latest_dir, f"{str(guild_id)}_reports_latest.json")
+
+    check_and_create_dirs([reports_dir, reports_latest_dir])
+
     with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(content, f)
+
+    with open(latest_file_path, 'w', encoding='utf-8') as f:
         json.dump(content, f)
