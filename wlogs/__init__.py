9b11e84d748507c0cd01639801be4407bc834773
Matt Matt <matthieu.chambery@gmail.com>
2023-04-08 22:51:45 +0200

    refactor to avoid creation of client in each notebook

--- wlogs/__init__.py
+++ wlogs/__init__.py
@@ -4,5 +4,4 @@ from .file_manager import save_guild_reports_to_json, load_latest_reports, save_
 from .queries import query_reports_on_page, query_report
 
 
-CLIENT = graphQL_client_from_json()
 GUILD_ID = 505778
