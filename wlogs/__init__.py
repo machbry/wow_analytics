e4342c3b9ec43144cb009a19bbaa59a3097c90e4
Matt Matt <matthieu.chambery@gmail.com>
2023-04-09 22:59:28 +0200

    change imports to match previous changes

--- wlogs/__init__.py
+++ wlogs/__init__.py
@@ -1,7 +1,8 @@
 from .api import graphQL_client_from_json
-from .files_storage import save_guild_reports_to_json, load_latest_reports, save_report_data_to_json, load_reports_data, \
-    save_df_to_csv, get_reports_code_saved
 from .queries import query_game_zones_on_page, query_game_specs, query_reports_on_page, query_report
+from .lakes import ReportsLake
+from .files import save_guild_reports_to_json, load_latest_reports, save_report_data_to_json, load_reports_data, \
+    save_df_to_csv, get_reports_code_saved
 
 
 GUILD_ID = 505778  # Rush N Wipe Again on Auberdine
