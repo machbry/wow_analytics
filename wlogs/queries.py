bbc8d3a3bc5c428127ea7dbeec577a87d112be82
Matt Matt <matthieu.chambery@gmail.com>
2023-04-09 15:31:18 +0200

    add queries to get game data (zones, class and specs)

--- wlogs/queries.py
+++ wlogs/queries.py
@@ -4,49 +4,79 @@ from .api import graphQL_client_from_json
 CLIENT = graphQL_client_from_json()
 
 
-def query_reports_on_page(guild_id, page, client=CLIENT) -> dict:
-    query = "query " + \
-                "{reportData " + \
-                    "{reports(guildID: " + str(guild_id) + ", page: " + str(page) + "){" + \
-                        "total per_page " + \
-                        "current_page " + \
-                        "from " + \
-                        "to " + \
-                        "last_page " + \
-                        "has_more_pages " + \
-                        "data{" + \
-                            "code " + \
-                            "title " + \
-                            "startTime " + \
-                            "endTime " + \
+def query_game_zones_on_page(page: int, client=CLIENT) -> dict:
+    query = "query {" +\
+                "gameData{" +\
+                    "zones(page: " + str(page) + "){" +\
+                        "total " +\
+                        "per_page " +\
+                        "current_page " +\
+                        "from " +\
+                        "to " +\
+                        "last_page " +\
+                        "has_more_pages " +\
+                        "data{" +\
+                            "id " +\
+                            "name }}}}"
+    return client.post(query)
+
+
+def query_game_specs(zone_id: int, client=CLIENT) -> dict:
+    query = "query {" +\
+                "gameData{" +\
+                    "classes(zone_id: " + str(zone_id) + "){" +\
+                        "id " +\
+                        "name " +\
+                        "specs{" +\
+                            "id " +\
+                            "name }}}}"
+    return client.post(query)
+
+
+def query_reports_on_page(guild_id: int, page: int, client=CLIENT) -> dict:
+    query = "query " +\
+                "{reportData " +\
+                    "{reports(guildID: " + str(guild_id) + ", page: " + str(page) + "){" +\
+                        "total " +\
+                        "per_page " +\
+                        "current_page " +\
+                        "from " +\
+                        "to " +\
+                        "last_page " +\
+                        "has_more_pages " +\
+                        "data{" +\
+                            "code " +\
+                            "title " +\
+                            "startTime " +\
+                            "endTime " +\
                             "segments}}}}"
     return client.post(query)
 
 
-def query_report(code, actor_type="Player", client=CLIENT) -> dict:
-    query = "query " + \
-                "{reportData " + \
-                    "{report(code: " + "\"" + code + "\"" + "){" + \
-                        "code " + \
-                        "title " + \
-                        "startTime " + \
-                        "endTime " + \
-                        "masterData {actors(type:" + "\"" + actor_type + "\"" + "){" + \
-                            "id " + \
-                            "name " + \
-                            "type " + \
-                            "subType}}" + \
-                        "fights {" + \
-                            "id " + \
-                            "name " + \
-                            "difficulty " + \
-                            "encounterID " + \
-                            "size " + \
-                            "hardModeLevel " + \
-                            "startTime " + \
-                            "endTime " + \
-                            "kill " + \
-                            "bossPercentage " + \
-                            "lastPhase " + \
+def query_report(code: str, actor_type="Player", client=CLIENT) -> dict:
+    query = "query " +\
+                "{reportData " +\
+                    "{report(code: " + "\"" + code + "\"" + "){" +\
+                        "code " +\
+                        "title " +\
+                        "startTime " +\
+                        "endTime " +\
+                        "masterData {actors(type:" + "\"" + actor_type + "\"" + "){" +\
+                            "id " +\
+                            "name " +\
+                            "type " +\
+                            "subType}}" +\
+                        "fights {" +\
+                            "id " +\
+                            "name " +\
+                            "difficulty " +\
+                            "encounterID " +\
+                            "size " +\
+                            "hardModeLevel " +\
+                            "startTime " +\
+                            "endTime " +\
+                            "kill " +\
+                            "bossPercentage " +\
+                            "lastPhase " +\
                             "averageItemLevel}}}}"
     return client.post(query)
