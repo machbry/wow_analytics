4a8d8eef79a5dfa5d8abda6ff740e37632b27a70
Matt Matt <matthieu.chambery@gmail.com>
2023-04-09 12:38:23 +0200

    better readability for grapgQL queries

--- wlogs/queries.py
+++ wlogs/queries.py
@@ -4,15 +4,49 @@ from .api import graphQL_client_from_json
 CLIENT = graphQL_client_from_json()
 
 
-def query_reports_on_page(guild_id, page, client=CLIENT) -> str:
-    query = "query {reportData {reports(guildID: "+str(guild_id)+", page: "+str(page)+")\
-     {total per_page current_page from to last_page has_more_pages data{code title startTime endTime segments}}}}"
+def query_reports_on_page(guild_id, page, client=CLIENT) -> dict:
+    query = "query " + \
+                "{reportData " + \
+                    "{reports(guildID: " + str(guild_id) + ", page: " + str(page) + "){" + \
+                        "total per_page " + \
+                        "current_page " + \
+                        "from " + \
+                        "to " + \
+                        "last_page " + \
+                        "has_more_pages " + \
+                        "data{" + \
+                            "code " + \
+                            "title " + \
+                            "startTime " + \
+                            "endTime " + \
+                            "segments}}}}"
     return client.post(query)
 
 
-def query_report(code, actor_type="Player", client=CLIENT):
-    query = "query {reportData {report(code: "+"\""+code+"\""+") {code title startTime endTime masterData \
-    {actors(type:"+"\""+actor_type+"\""+") {id name type subType}} \
-    fights {id name difficulty encounterID size hardModeLevel startTime endTime kill bossPercentage lastPhase \
-    averageItemLevel}}}}"
+def query_report(code, actor_type="Player", client=CLIENT) -> dict:
+    query = "query " + \
+                "{reportData " + \
+                    "{report(code: " + "\"" + code + "\"" + "){" + \
+                        "code " + \
+                        "title " + \
+                        "startTime " + \
+                        "endTime " + \
+                        "masterData {actors(type:" + "\"" + actor_type + "\"" + "){" + \
+                            "id " + \
+                            "name " + \
+                            "type " + \
+                            "subType}}" + \
+                        "fights {" + \
+                            "id " + \
+                            "name " + \
+                            "difficulty " + \
+                            "encounterID " + \
+                            "size " + \
+                            "hardModeLevel " + \
+                            "startTime " + \
+                            "endTime " + \
+                            "kill " + \
+                            "bossPercentage " + \
+                            "lastPhase " + \
+                            "averageItemLevel}}}}"
     return client.post(query)
