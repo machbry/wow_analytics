bc96ed0cce8c6675fd1778fccf40e7941e696a82
Matt Matt <matthieu.chambery@gmail.com>
2023-04-10 18:10:59 +0200

    get latest reports method

--- wlogs/lakes/reports.py
+++ wlogs/lakes/reports.py
@@ -1,6 +1,14 @@
+import pandas as pd
+import json
+
 from wlogs.datalake import StorageFormat, Lake
 
 
 class ReportsLake(Lake):
     def __init__(self):
         super().__init__(name="reports", storage_format=StorageFormat.json, latest=True)
+
+    def get_latest(self) -> pd.DataFrame:
+        with open(self.latest_file_path, 'r', encoding=self._encoding) as f:
+            reports = json.load(f)
+        return pd.DataFrame(reports)
