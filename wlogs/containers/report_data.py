9290e316c9edb89ee1e3d3471a71476c9d7d5f60
Matthieu Chambery <mchambery@betomorrow.com>
2023-04-13 13:07:46 +0200

    renaming to extract data from container

--- wlogs/containers/report_data.py
+++ wlogs/containers/report_data.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 from wlogs.storage import JSONFormat, BatchContainer, DATA_DIRECTORY
 
@@ -7,7 +8,7 @@ class ReportDataContainer(BatchContainer):
     def __init__(self):
         super().__init__(name="report_data", storage_file_format=JSONFormat(), parent=DATA_DIRECTORY)
 
-    def extract_data(self) -> dict:
+    def extract(self) -> dict:
         reports_data_files = self.all_files_stored()
         reports_data = {}
         for json_path in reports_data_files:
@@ -15,3 +16,6 @@ class ReportDataContainer(BatchContainer):
             with open(json_path, 'r', encoding=self._storage_file_format.encoding) as f:
                 reports_data[code] = json.load(f)
         return reports_data
+
+    def reports_codes_stored(self) -> List[str]:
+        return self.all_filenames_stored()
--- wlogs/containers/reports.py
+++ wlogs/containers/reports.py
@@ -8,7 +8,7 @@ class ReportsContainer(LatestContainer):
     def __init__(self):
         super().__init__(name="reports", storage_file_format=JSONFormat(), parent=DATA_DIRECTORY)
 
-    def get_latest(self) -> pd.DataFrame:
+    def extract(self) -> pd.DataFrame:
         with open(self.latest_file_path, 'r', encoding=self._storage_file_format.encoding) as f:
             reports = json.load(f)
         return pd.DataFrame(reports)
