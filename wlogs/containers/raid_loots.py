ce5f060eeabff292e28a59162f9fe70444059c89
Matt Matt <matthieu.chambery@gmail.com>
2023-04-15 16:51:49 +0200

    get all items id stored in raid loots container

--- wlogs/containers/raid_loots.py
+++ wlogs/containers/raid_loots.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from typing import List
 from wlogs.storage import CSVFormat, LatestContainer, DATA_DIRECTORY
 
 
@@ -9,3 +9,8 @@ class RaidLootsContainer(LatestContainer):
 
     def extract(self) -> pd.DataFrame:
         return super().extract()
+
+    def get_all_items_id(self) -> List[int]:
+        df = self.extract()
+        return [int(item_id) for item_id in df['item_id'].unique()]
+
