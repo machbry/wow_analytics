9f1035935760a06dbb03f784d32309cabd15eddc
CHAMBERY Matthieu <matthieu.chambery@gmail.com>
2023-08-04 15:21:32 +0200

    auto disable network calls from requests in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pytest
 from wlogs.storage import BatchContainer, JSONFormat
+import requests
 
 TESTS_DATA_PATH = Path(__file__).parent.resolve() / "data"
 TESTS_DATA_PATH.mkdir(parents=True, exist_ok=True)
@@ -11,3 +12,14 @@ def json_batch_container():
     return BatchContainer(**{"name": "test_batch",
                              "storage_file_format": JSONFormat(),
                              "parent": TESTS_DATA_PATH})
+
+
+@pytest.fixture(autouse=True)
+def disable_network_calls(monkeypatch):
+    def stunted_get():
+        raise RuntimeError("Network access not allowed during testing!")
+    monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
+
+    def stunted_post():
+        raise RuntimeError("Network access not allowed during testing!")
+    monkeypatch.setattr(requests, "post", lambda *args, **kwargs: stunted_post())
